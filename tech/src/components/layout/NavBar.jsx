b2155f9150f6cd999fcdd3950d3efa92096968e0
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-07-01 13:28:33 -0300

    Update NavBar.jsx

---
+++ tech/src/components/layout/NavBar.jsx
@@ -0,0 +1,21 @@
+import Link from '../items/Link'
+
+function NavBar() {
+    return(
+        <nav className="h-10 justify-center content-center border px-5">
+
+            <Container>
+                <ul className="flex justify-between max-w-7xl">
+                    <li><Link href="#" text="DLM"></Link></li>
+                    <div className='w-1/6'></div>
+                    <li><Link href="#" target="_blank" text="Sobre"></Link></li>
+                    <li><Link href="" target="_blank" text="Pesquisar"></Link></li>
+                    <li><Link href="#" target="_blank" text="Carrinho"></Link></li>
+                </ul>
+            </Container>
+
+        </nav>
+    )
+}
+
+export default NavBar
