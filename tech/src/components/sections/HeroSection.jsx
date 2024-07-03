f56c08bd83eafe28753c7e998bd43319c909cc8e
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-07-03 16:42:43 -0300

    Hero Section working!

--- tech/src/components/sections/HeroSection.jsx
+++ tech/src/components/sections/HeroSection.jsx
@@ -2,17 +2,25 @@ import Section from "./Section"
 
 function HeroSection() {
     return(
-        <Section customclass="flex h-[600px] pt-10">
+        <Section customclass=" block md:flex h-[600px] pt-10">
 
             <article className="p-[10%]">
+
                 <h1 className="text-5xl pt-10">
                     A melhor qualidade pelo menor preço
                 </h1>
+
             </article>
 
             <aside>
-                <div className="size-[560px] bg-black ">
-                    <button>saiba mais</button>
+
+                <div className=" absolute top-10 right-0 md:static w-full h-[560px] sm:size-[560px] bg-opacity-75 bg-black flex justify-center items-center
+             ">
+
+                    <button className=" bg-black w-10">
+
+                        saiba mais
+                    </button>
                 </div>
                 
             </aside>
